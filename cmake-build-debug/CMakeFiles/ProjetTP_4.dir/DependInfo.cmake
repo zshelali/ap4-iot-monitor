fdd046cbc533da7ab5cbaad69e881c796bb29cb9
zshelali <ali.hajeri24@gmail.com>
2024-11-09 23:18:56 +0100

    6791

---
+++ cmake-build-debug/CMakeFiles/ProjetTP_4.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/zshelali/CLionProjects/ap4-iot-monitor/Source/Luxmeter.cpp" "CMakeFiles/ProjetTP_4.dir/Source/Luxmeter.cpp.o" "gcc" "CMakeFiles/ProjetTP_4.dir/Source/Luxmeter.cpp.o.d"
+  "/Users/zshelali/CLionProjects/ap4-iot-monitor/Source/Scheduler.cpp" "CMakeFiles/ProjetTP_4.dir/Source/Scheduler.cpp.o" "gcc" "CMakeFiles/ProjetTP_4.dir/Source/Scheduler.cpp.o.d"
+  "/Users/zshelali/CLionProjects/ap4-iot-monitor/Source/Sensor.cpp" "CMakeFiles/ProjetTP_4.dir/Source/Sensor.cpp.o" "gcc" "CMakeFiles/ProjetTP_4.dir/Source/Sensor.cpp.o.d"
+  "/Users/zshelali/CLionProjects/ap4-iot-monitor/Source/Server.cpp" "CMakeFiles/ProjetTP_4.dir/Source/Server.cpp.o" "gcc" "CMakeFiles/ProjetTP_4.dir/Source/Server.cpp.o.d"
+  "/Users/zshelali/CLionProjects/ap4-iot-monitor/Source/Thermometer.cpp" "CMakeFiles/ProjetTP_4.dir/Source/Thermometer.cpp.o" "gcc" "CMakeFiles/ProjetTP_4.dir/Source/Thermometer.cpp.o.d"
+  "/Users/zshelali/CLionProjects/ap4-iot-monitor/Source/main.cpp" "CMakeFiles/ProjetTP_4.dir/Source/main.cpp.o" "gcc" "CMakeFiles/ProjetTP_4.dir/Source/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/ProjetTP_4.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProjetTP_4.dir/Source/Luxmeter.cpp.o"
+  "CMakeFiles/ProjetTP_4.dir/Source/Luxmeter.cpp.o.d"
+  "CMakeFiles/ProjetTP_4.dir/Source/Scheduler.cpp.o"
+  "CMakeFiles/ProjetTP_4.dir/Source/Scheduler.cpp.o.d"
+  "CMakeFiles/ProjetTP_4.dir/Source/Sensor.cpp.o"
+  "CMakeFiles/ProjetTP_4.dir/Source/Sensor.cpp.o.d"
+  "CMakeFiles/ProjetTP_4.dir/Source/Server.cpp.o"
+  "CMakeFiles/ProjetTP_4.dir/Source/Server.cpp.o.d"
+  "CMakeFiles/ProjetTP_4.dir/Source/Thermometer.cpp.o"
+  "CMakeFiles/ProjetTP_4.dir/Source/Thermometer.cpp.o.d"
+  "CMakeFiles/ProjetTP_4.dir/Source/main.cpp.o"
+  "CMakeFiles/ProjetTP_4.dir/Source/main.cpp.o.d"
+  "ProjetTP_4"
+  "ProjetTP_4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProjetTP_4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
