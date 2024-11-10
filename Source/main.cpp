87ad14a0cf8a155243087bda066c9af17fc84b6e
zshelali <ali.hajeri24@gmail.com>
2024-11-10 03:36:26 +0100

    FINAL ?

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,3 +1,18 @@
+/* =========================
+* Ali Hajeri - AP4A - A24 - UTBM
+ * Project made and tested with CLION - macOS 15.1
+ *
+ * Updates every :
+ * Thermometer : 10s
+ * Luxmeter : 1s
+ * Decibelmeter : 3s
+ *
+ * Generates a CSV file with the logs (the separator is a semicolon ";" not a comma ",")
+ * Located at :
+ * ap4-iot-monitor/FileLogs/server-name.csv
+ * =========================*/
+
+
 #include <iostream>
 #include <string>
 #include <vector>
@@ -11,19 +26,6 @@
 
 using namespace std;
 
-/* =========================
- * Ali Hajeri - AP4A - A24 - UTBM
- * Project made and tested with CLION - macOS 15.1
- *
- * Updates every :
- * Thermometer : 10s
- * Luxmeter : 1s
- * Decibelmeter : 3s
- *
- * Generates a CSV file with the logs (the separator is a semicolon ";" not a comma ",")
- * Located at :
- * ap4-iot-monitor/FileLogs/server-name.csv
- * =========================*/
 
 
 int main() {
